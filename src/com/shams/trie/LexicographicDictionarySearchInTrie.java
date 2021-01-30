80e3e4a4e1fc49f8b35d41a2eeff847fb45bdbae
i334072 <shams.nalband.thabrez@sap.com>
2021-01-30 19:39:15 +0530

    Adding lexicographic order dictionary search using trie

---
+++ src/com/shams/trie/LexicographicDictionarySearchInTrie.java
@@ -0,0 +1,60 @@
+/**
+ * https://www.geeksforgeeks.org/sorting-array-strings-words-using-trie/
+ */
+
+package com.shams.trie;
+
+public class LexicographicDictionarySearchInTrie {
+  private static final int MAX_INT = 26;
+
+  public static class Trie {
+    int index;
+    Trie[] children = new Trie[MAX_INT];
+
+    Trie() {
+      for (int i = 0; i < MAX_INT; ++i) {
+        children[i] = null;
+        index = -1;
+      }
+    }
+  }
+
+
+
+  public static void insert(Trie root, String s, int stringIndexInArray) {
+    Trie node = root;
+    for (int i = 0; i < s.length(); ++i) {
+      int charIndexInString = s.charAt(i) - 'a';
+      if (node.children[charIndexInString] == null) {
+        node.children[charIndexInString] = new Trie();
+      }
+      node = node.children[charIndexInString];
+    }
+    node.index = stringIndexInArray;
+  }
+
+  private static void search(Trie node, String[] arr) {
+    if (node == null)
+      return;
+
+    for(int i = 0; i < MAX_INT; ++i) {
+      if(node.children[i] != null) {
+        if(node.children[i].index != -1) {
+          System.out.println(arr[node.children[i].index]);
+        }
+      }
+      search(node.children[i], arr);
+    }
+  }
+
+  public static void main(String[] args) {
+    Trie root = new Trie();
+    String[] arr = {"geeks", "for", "geeks", "a", "portal",
+        "to", "learn", "can", "be", "computer",
+        "science", "zoom", "yup", "fire", "in", "data"};
+    for (int i = 0; i < arr.length; ++i) {
+      insert(root, arr[i], i);
+    }
+    search(root, arr);
+  }
+}
