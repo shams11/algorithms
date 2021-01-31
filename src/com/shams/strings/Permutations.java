984a29ac3b0889e4ed0dab5831051087fd926546
i334072 <shams.nalband.thabrez@sap.com>
2021-01-31 09:45:28 +0530

    Adding string permutations

---
+++ src/com/shams/strings/Permutations.java
@@ -0,0 +1,24 @@
+/**
+ * https://www.geeksforgeeks.org/time-complexity-permutations-string/
+ * */
+
+package com.shams.strings;
+
+public class Permutations {
+
+  public static void main(String[] args) {
+    perm("abc", "");
+  }
+
+  static void perm(String str, String prefix) {
+    if (str.length() == 0) {
+      System.out.println(prefix);
+    } else {
+      for (int i = 0; i < str.length(); i++) {
+        String rem = str.substring(0, i) +
+            str.substring(i + 1);
+        perm(rem, prefix + str.charAt(i));
+      }
+    }
+  }
+}
