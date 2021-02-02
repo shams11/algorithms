8f10f2433fbfc98a4087559d1c9dccc8c7cb958d
i334072 <shams.nalband.thabrez@sap.com>
2021-02-02 08:06:54 +0530

    Adding subset sum problem

---
+++ src/com/shams/dp/Knapsack.java
@@ -0,0 +1,7 @@
+/**
+ * https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
+ * */
+package com.shams.dp;
+
+public class Knapsack {
+}
---
+++ src/com/shams/dp/SubsetSum.java
@@ -0,0 +1,59 @@
+/**
+ * https://www.geeksforgeeks.org/subset-sum-problem-dp-25/
+ */
+
+package com.shams.dp;
+
+public class SubsetSum {
+
+  public static void main(String[] args) {
+    int arr[] = {3, 34, 4, 12, 5, 2};
+    int sum = 119;
+    int n = arr.length;
+    //System.out.println(subSetSum(arr, sum, n));
+    System.out.println(subSetSumTopdown(arr, sum, n));
+  }
+
+
+  private static boolean subSetSumTopdown(int[] arr, int sum, int n) {
+    boolean[][] t = new boolean[n + 1][sum + 1];
+
+    //Initialization
+    for (int i = 0; i < n + 1; ++i) {
+      for (int j = 0; j < sum + 1; ++j) {
+        if (i == 0) {
+          t[i][j] = false;
+        }
+        if (j == 0) {
+          t[i][j] = true;
+        }
+      }
+    }
+
+    for (int i = 1; i < n + 1; ++i) {
+      for (int j = 1; j < sum + 1; ++j) {
+        if (arr[i - 1] > j) {
+          t[i][j] = t[i - 1][j];
+        } else {
+          t[i][j] = t[i - 1][j - arr[i - 1]] || t[i - 1][j];
+        }
+      }
+    }
+    return t[n][sum];
+  }
+
+  private static boolean subSetSum(int[] arr, int sum, int n) {
+    if (n == 0) {
+      return false;
+    }
+    if (sum == 0) {
+      return true;
+    }
+    if (arr[n - 1] > sum) {
+      return subSetSum(arr, sum, n - 1);
+    } else {
+      return subSetSum(arr, sum - arr[n - 1], n - 1) ||
+          subSetSum(arr, sum, n - 1);
+    }
+  }
+}
