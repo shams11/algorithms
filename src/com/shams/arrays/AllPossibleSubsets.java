c4034a1366e7cedf99869f94cb3908df42c8a126
i334072 <shams.nalband.thabrez@sap.com>
2021-02-03 09:46:19 +0530

    Adding count of subsets with given diff

---
+++ src/com/shams/arrays/AllPossibleSubsets.java
@@ -0,0 +1,35 @@
+/**
+ * https://leetcode.com/problems/subsets/discuss/1031486/Javaor-Recursion-or-100
+ */
+
+package com.shams.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllPossibleSubsets {
+  public static void main(String[] args) {
+    int[] nums = {1, 2, 3};
+    List<List<Integer>> result = subsets(nums);
+    result.forEach(System.out::println);
+  }
+
+  static List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> list = new ArrayList<>();
+    createSubsets(list, new ArrayList<>(), nums, 0);
+    return list;
+  }
+
+  static void createSubsets(
+      List<List<Integer>> list,
+      List<Integer> resultList,
+      int[] nums, int start) {
+
+    list.add(new ArrayList<>(resultList));
+    for (int i = start; i < nums.length; i++) {
+      resultList.add(nums[i]);
+      createSubsets(list, resultList, nums, i + 1);
+      resultList.remove(resultList.size() - 1);
+    }
+  }
+}
---
+++ src/com/shams/dp/TargetSum.java
@@ -0,0 +1,48 @@
+/**
+ * https://leetcode.com/problems/target-sum/
+ * Same code ad Count of subsets with given diff
+ * */
+package com.shams.dp;
+
+public class TargetSum {
+  public static void main(String[] args) {
+    int[] a = {1, 1, 1, 1, 1};
+    int diff = 3;
+    System.out.println(countOfSubSetsEithGivenDiff(a, diff, a.length));
+  }
+
+  private static int countOfSubSetsEithGivenDiff(int[] a, int diff, int n) {
+    int totalSum = 0;
+    for (int i = 0; i < n; ++i) {
+      totalSum += a[i];
+    }
+    int sum = (diff + totalSum) / 2;
+    return countSubsetWithGiveSum(a, sum, n);
+  }
+
+  private static int countSubsetWithGiveSum(int[] a, int sum, int n) {
+
+    int[][] t = new int[n + 1][sum + 1];
+    for (int i = 0; i < n + 1; i++) {
+      for (int j = 0; j < sum; j++) {
+        if (i == 0) {
+          t[i][j] = 0;
+        }
+        if (j == 0) {
+          t[i][j] = 1;
+        }
+      }
+    }
+
+    for (int i = 1; i < n + 1; i++) {
+      for (int j = 1; j < sum + 1; j++) {
+        if (a[i - 1] > j) {
+          t[i][j] = t[i - 1][j];
+        } else {
+          t[i][j] = t[i - 1][j] + t[i - 1][j - a[i - 1]];
+        }
+      }
+    }
+    return t[n][sum];
+  }
+}
