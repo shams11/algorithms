f744b5a4562d31d3af75b881bf66f8baecfddac8
i334072 <shams.nalband.thabrez@sap.com>
2021-02-05 11:43:02 +0530

    optimized top k frequent words

--- src/com/shams/strings/TopKFrequentWords.java
+++ src/com/shams/strings/TopKFrequentWords.java
@@ -6,7 +6,6 @@
 package com.shams.strings;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -17,7 +16,7 @@ import java.util.PriorityQueue;
 public class TopKFrequentWords {
   public static void main(String[] args) {
 
-    String[] s = {"i", "love", "leetcode", "i", "love", "leetcode"};
+    String[] s = {"i", "love", "leetcode", "i", "love", "leetcode", "leetcode", "shams", "shams", "shams", "shams"};
     int k = 2;
     List<String> result = topKFrequent1(s, k);
     System.out.println(result);
@@ -28,16 +27,15 @@ public class TopKFrequentWords {
     for (String word : words) {
       map.put(word, map.getOrDefault(word, 0) + 1);
     }
-    PriorityQueue<String> pq = new PriorityQueue<>(new Comparator<String>() {
-      @Override
-      public int compare(String word1, String word2) {
-        int frequency1 = map.get(word1);
-        int frequency2 = map.get(word2);
-        if (frequency1 == frequency2) {
-          return word2.compareTo(word1);
-        }
-        return frequency1 - frequency2;
+    PriorityQueue<String> pq = new PriorityQueue<>((word1, word2) -> {
+      int frequency1 = map.get(word1);
+      int frequency2 = map.get(word2);
+      if (frequency1 == frequency2) {
+        // if frequency is same, then order words alphabetically
+        return word2.compareTo(word1);
       }
+      // else order by frequency
+      return frequency1 - frequency2;
     });
     for (Map.Entry<String, Integer> e : map.entrySet()) {
       pq.add(e.getKey());
@@ -51,24 +49,5 @@ public class TopKFrequentWords {
     }
     Collections.reverse(result);
     return result;
-  }
-
-  private static List<String> topKFrequent(String[] words, int k) {
-    Map<String, Integer> map = new HashMap<String, Integer>();
-    List<String> ans = new ArrayList<String>();
-    for (String word : words) {
-      map.put(word, map.getOrDefault(word, 0) + 1);
-    }
-    List<String> result = new ArrayList<>(map.keySet());
-    System.out.println(result);
-    result.sort((word1, word2) -> {
-      int comparision = map.get(word1).compareTo(map.get(word2));
-      if (comparision == 0) {
-        return word1.compareTo(word2);
-      }
-      return comparision;
-    });
-
-    return result.subList(0, k);
   }
 }
