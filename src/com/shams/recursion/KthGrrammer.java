1cc9780b0ae0c9636f5ba1ba66c1b4d150a35b35
i334072 <shams.nalband.thabrez@sap.com>
2021-02-13 13:49:49 +0530

    Adding kth grammer

---
+++ src/com/shams/recursion/KthGrrammer.java
@@ -0,0 +1,27 @@
+/**
+ * https://leetcode.com/problems/k-th-symbol-in-grammar/
+ * https://www.youtube.com/watch?v=5P84A0YCo_Y&list=PL_z_8CaSLPWeT1ffjiImo0sYTcnLzo-wY&index=10&ab_channel=AdityaVerma
+ */
+
+package com.shams.recursion;
+
+public class KthGrrammer {
+  public static void main(String[] args) {
+    System.out.println(kthGrammar(2, 2));
+  }
+
+  public static int kthGrammar(int N, int K) {
+    if (N == 1 && K == 1)
+      return 0;
+    int mid = (int) (Math.pow(2, N - 1) / 2);
+    if (K <= mid) {
+      return kthGrammar(N - 1, K);
+    } else {
+      int result = (kthGrammar(N - 1, (int) (K - mid)));
+      if (result == 0)
+        return 1;
+      else
+        return 0;
+    }
+  }
+}
