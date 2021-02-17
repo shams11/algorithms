5a915c1cbeca6d3e03f3372d542cab471c59c831
i334072 <shams.nalband.thabrez@sap.com>
2021-02-17 12:12:17 +0530

    Refactored kth bit in nth binary string

--- src/com/shams/recursion/KthBitInNthBinaryString.java
+++ src/com/shams/recursion/KthBitInNthBinaryString.java
@@ -10,7 +10,7 @@ public class KthBitInNthBinaryString {
   }
 
   private static char solve(int n, int k) {
-    if (n == 1 && k==1) {
+    if (n == 1 && k == 1) {
       return '0';
     }
     int mid = (int) (Math.pow(2, n) - 1) / 2;
