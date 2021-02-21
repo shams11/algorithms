ec828fc5086aabad24dff0ee9e87604aadb0f20a
i334072 <shams.nalband.thabrez@sap.com>
2021-02-21 18:15:29 +0530

    Adding Rod cutting with given pattern to find min cost

--- src/com/shams/dp/RodCutting.java
+++ src/com/shams/dp/RodCutting.java
@@ -1,39 +1,43 @@
 /**
  * https://leetcode.com/problems/minimum-cost-to-cut-a-stick/
+ * https://www.youtube.com/watch?v=8uvWmiawJ8Q&ab_channel=AbrarSher
  */
 
 package com.shams.dp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class RodCutting {
   public static void main(String[] args) {
-    int[] cuts = {1, 3, 4, 5};
+    int[] cuts = {3, 5, 1, 4};
     int N = 7;
-    System.out.println(minCost(cuts, N, cuts.length));
+  //   Output: 16
+
+//    int N = 9;
+//    int[] cuts = {5,6,1,4,2};
+   // Output: 22
+    System.out.println(minCost(cuts, N));
   }
 
-  public static int minCost(int[] cuts, int N, int n) {
-    int[][] t = new int[n + 1][N + 1];
+  public static int minCost(int[] cuts, int n) {
 
-    for (int i = 0; i < n + 1; i++) {
-      for (int j = 0; j < N + 1; j++) {
-        if (i == 0) {
-          t[i][j] = 0;
-        }
-        if(j == 0) {
-          t[i][j] = 1;
+
+    int[] c = new int[cuts.length + 2];
+    int[][] t = new int[c.length][c.length];
+    for (int i = 0; i < cuts.length; i++) {
+      c[i + 1] = cuts[i];
+    }
+    c[c.length - 1] = n;
+    Arrays.sort(c);
+    for(int i = 0; i < c.length; i++) {
+      for(int j = i-1; j >= 0 ; j--) {
+        for(int k = i-1; k > j; k--) {
+          t[j][i] =  Math.min(t[j][i] == 0 ? Integer.MAX_VALUE : t[j][i] , (t[j][k] + t[k][i]) + (c[i] - c[j]));
         }
       }
     }
-    t[0][0] = 0;
-    for (int i = 1; i < n + 1; i++) {
-      for (int j = 1; j < N + 1; j++) {
-        if (cuts[i - 1] > j) {
-          t[i][j] = t[i - 1][j];
-        } else {
-          t[i][j] = Math.min(t[i - 1][j],  j + t[i-1][j - cuts[i - 1]]);
-        }
-      }
-    }
-    return t[n][N];
+    return t[0][c.length-1];
   }
 }
