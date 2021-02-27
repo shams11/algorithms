9417452d84a27672f6628ef1a6a18a7e0f22462d
i334072 <shams.nalband.thabrez@sap.com>
2021-02-27 20:52:46 +0530

    Adding shoertest common supersequence

---
+++ src/com/shams/dp/ShortestCommonSupersequence.java
@@ -0,0 +1,44 @@
+/***
+ *
+ * https://www.youtube.com/watch?v=823Grn4_dCQ&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=24&ab_channel=AdityaVerma
+ * https://www.geeksforgeeks.org/shortest-common-supersequence/
+ * */
+
+package com.shams.dp;
+
+public class ShortestCommonSupersequence {
+  static int[][] t;
+
+  public static void main(String[] args) {
+    String s1 = "AGGTAB";
+    String s2 = "GXTXAYB";
+
+    int m = s1.length();
+    int n = s2.length();
+
+    t = new int[m + 1][n + 1];
+    int l = lcs(s1, s2, m, n);
+
+    System.out.println("length of shortest common super sequence : " + (m + n - l));
+  }
+
+  private static int lcs(String x, String y, int m, int n) {
+    for (int i = 0; i < m + 1; i++) {
+      for (int j = 0; j < n + 1; j++) {
+        if (i == 0 || j == 0) {
+          t[i][j] = 0;
+        }
+      }
+    }
+    for (int i = 1; i < m + 1; i++) {
+      for(int j = 1; j < n+1; j++) {
+        if(x.charAt(i-1) == y.charAt(j-1)) {
+          t[i][j] = t[i-1][j-1] + 1;
+        } else {
+          t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
+        }
+      }
+    }
+    return t[m][n];
+  }
+}
