f019a3aa7e81308d56dd9dc6c24611dee8086da9
i334072 <shams.nalband.thabrez@sap.com>
2021-03-20 21:19:08 +0530

    Adding nsr

--- src/com/shams/stack/NextSmallerToRightInArray.java
+++ src/com/shams/stack/NextSmallerToRightInArray.java
@@ -1,8 +1,39 @@
 
 package com.shams.stack;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
 public class NextSmallerToRightInArray {
   public static void main(String[] args) {
+    int[] a = {1, 3, 2, 4};
+    // Output : [-1, 2, -1, -1]
+    System.out.println(nsr(a));
+  }
 
+  private static List<Integer> nsr(int[] a) {
+    List<Integer> list = new ArrayList<>();
+    Stack<Integer> s = new Stack<>();
+    for (int i = a.length - 1; i >= 0; i--) {
+      if (s.isEmpty()) {
+        list.add(-1);
+      } else if (a[i] > s.peek()) {
+        list.add(s.peek());
+      } else if (a[i] < s.peek()) {
+        while (!s.isEmpty() && a[i] < s.peek()) {
+          s.pop();
+        }
+        if (s.isEmpty()) {
+          list.add(-1);
+        } else {
+          list.add(s.peek());
+        }
+      }
+      s.push(a[i]);
+    }
+    Collections.reverse(list);
+    return list;
   }
 }
