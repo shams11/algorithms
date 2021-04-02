d52b965e3fbe6fd0e82a86d88954fb0d87f2e1da
i334072 <shams.nalband.thabrez@sap.com>
2021-04-02 17:37:52 +0530

    Refactored ceil of an element in sorted array

--- src/com/shams/binarysearch/CeilOfAnEleInSortedArray.java
+++ src/com/shams/binarysearch/CeilOfAnEleInSortedArray.java
@@ -13,7 +13,12 @@ public class CeilOfAnEleInSortedArray {
     // For x = 1:    floor  = 1,  ceil  = 1
     // For x = 5:    floor  = 2,  ceil  = 8
     // For x = 20:   floor  = 19,  ceil doesn't exist in array
-    System.out.println(ceilOfX(a, 0, a.length - 1, x));
+    // System.out.println(ceilOfX(a, 0, a.length - 1, x));
+
+    char[] c = {'a', 'c', 'f', 'h'};
+    char key = 'f';
+    // Output : h
+    System.out.println(getNextCharOfKey(c, 0, c.length - 1, key));
   }
 
   private static int ceilOfX(int[] a, int start, int end, int x) {
@@ -27,6 +32,22 @@ public class CeilOfAnEleInSortedArray {
         res = a[mid];
         end = mid - 1;
       } else if (a[mid] < x) {
+        start = mid + 1;
+      }
+    }
+    return res;
+  }
+
+  //char[] c = {'a', 'c', 'f', 'h'};
+  static char getNextCharOfKey(char[] a, int start, int end, char key) {
+    int mid;
+    char res = (char) -1;
+    while (start <= end) {
+      mid = start + (end - start) / 2;
+      if (a[mid] > key) {
+        res = a[mid];
+        end = mid - 1;
+      } else if (a[mid] <= key) {
         start = mid + 1;
       }
     }
