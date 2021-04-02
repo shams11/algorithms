544738d6843295eedf2e812b756fff78c062222d
i334072 <shams.nalband.thabrez@sap.com>
2021-04-02 08:48:54 +0530

    Refactored rotation count in sorted array

--- src/com/shams/strings/RotationCountInSortedArray.java
+++ src/com/shams/strings/RotationCountInSortedArray.java
@@ -12,7 +12,7 @@ public class RotationCountInSortedArray {
     // int[] a = {7, 9, 11, 12, 15};
     // Output : 0
 
-    int[] a = {15, 18, 2, 3, 6, 12};
+    int[] a = {4, 5, 6, 7, 0, 1, 2};
     // Output : 2
     System.out.println(rotationCount(a, 0, a.length - 1, a.length));
   }
@@ -20,16 +20,19 @@ public class RotationCountInSortedArray {
   private static int rotationCount(int[] a, int start, int end, int n) {
     int mid, next, prev;
     while (start <= end) {
+      if (a[start] <= a[end]) {
+        return start;
+      }
       mid = start + (end - start) / 2;
       next = (mid + 1) % n;
       prev = (mid + n - 1) % n;
       if (a[mid] <= a[next] && a[mid] <= a[prev]) {
         return mid;
       }
-      if (a[start] > a[mid]) {
-        end = mid - 1;
-      } else {
+      if (a[start] <= a[mid]) {
         start = mid + 1;
+      } else if (a[mid] <= end) {
+        end = mid - 1;
       }
     }
     return 0;
