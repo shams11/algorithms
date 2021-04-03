34e80843ed22ce8dea1e3e34db13dd0940e083b9
i334072 <shams.nalband.thabrez@sap.com>
2021-04-03 08:12:10 +0530

    Adding min diff element in sorted array for a give n key

---
+++ src/com/shams/binarysearch/MinDiffElemInSortedArray.java
@@ -0,0 +1,35 @@
+/***
+ * https://www.youtube.com/watch?v=3RhGdmoF_ac&list=PL_z_8CaSLPWeYfhtuKHj-9MpYb6XQJ_f2&index=15&ab_channel=AdityaVerma
+ * */
+
+package com.shams.binarysearch;
+
+public class MinDiffElemInSortedArray {
+  public static void main(String[] args) {
+    int[] a = {1, 3, 8, 10, 15};
+    int k = 25;
+    System.out.println(getMinDiffForKey(a, 0, a.length - 1, k));
+  }
+
+  private static int getMinDiffForKey(int[] a, int start, int end, int key) {
+    if (key < a[start]) {
+      return a[0] - key;
+    }
+    if (key > a[end]) {
+      return key - a[end];
+    }
+    int mid;
+    while (start <= end) {
+      mid = start + (end - start) / 2;
+      if (a[mid] == key) {
+        return 0;
+      }
+      if (key < a[mid]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+    return Math.min(Math.abs(a[end] - key), Math.abs(a[start] - key));
+  }
+}
