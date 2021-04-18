6b60064f43b23e22c8c27a892391fa0ac47079d0
i334072 <shams.nalband.thabrez@sap.com>
2021-04-18 15:26:22 +0530

    Adding Maximal square

---
+++ src/com/shams/dp/MaximalSquare.java
@@ -0,0 +1,40 @@
+/***
+ * https://leetcode.com/problems/maximal-square/
+ * https://www.youtube.com/watch?v=RElcqtFYTm0&ab_channel=TECHDOSE
+ * */
+package com.shams.dp;
+
+public class MaximalSquare {
+  public static void main(String[] args) {
+    int[][] a = {
+        {1, 0, 1, 0, 0},
+        {1, 0, 1, 1, 1},
+        {1, 1, 1, 1, 1},
+        {1, 0, 0, 1, 0}
+    };
+    System.out.println(maximalSquare(a));
+  }
+
+  public static int maximalSquare(int[][] matrix) {
+    if (matrix == null || matrix.length == 0) return 0;
+
+    int m = matrix.length, n = matrix[0].length;
+    int largest = 0;
+    int[][] t = new int[m + 1][n + 1];
+    for (int i = 1; i < m + 1; i++) {
+      for (int j = 1; j < n + 1; j++) {
+        if (matrix[i - 1][j - 1] == 1) {
+          t[i][j] = 1 + min(t[i - 1][j - 1], t[i][j - 1], t[i - 1][j]);
+          largest = Math.max(largest, t[i][j]);
+        }
+      }
+    }
+    return largest * largest;
+  }
+
+  private static int min(int x, int y, int z) {
+    if (x <= y && x <= z) return x;
+    if (y <= x && y <= z) return y;
+    return z;
+  }
+}
