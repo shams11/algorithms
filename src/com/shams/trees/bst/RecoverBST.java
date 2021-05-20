1d571fd22f590df4470ea3d89b70b0ccaef365e5
i334072 <shams.nalband.thabrez@sap.com>
2021-05-20 20:08:13 +0530

    Adding Recover BST

---
+++ src/com/shams/trees/bst/RecoverBST.java
@@ -0,0 +1,65 @@
+/**
+ * https://leetcode.com/problems/recover-binary-search-tree/
+ */
+
+package com.shams.trees.bst;
+
+import com.shams.trees.binarytrees.ConstructBTFromInAndLevelOrder;
+
+public class RecoverBST {
+
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(5);
+    root.left = new TreeNode(3);
+    root.right = new TreeNode(2);
+    root.left.left = new TreeNode(6);
+    root.left.right = new TreeNode(4);
+    recoverTree(root);
+    printTree(root);
+  }
+
+  private static void printTree(TreeNode root) {
+    if (root == null) return;
+    printTree(root.left);
+    System.out.print(root.val + " ");
+    printTree(root.right);
+  }
+
+  public static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+  }
+
+  // Threre are 2 ndoes where the BST property is failing. figure out those 2 nodes
+  static TreeNode prev = null;
+  static TreeNode firstElement = null;
+  static TreeNode secondElement = null;
+
+  public static void recoverTree(TreeNode root) {
+    inorder(root);
+    // swap firstElement and secondElement
+    int temp = firstElement.val;
+    firstElement.val = secondElement.val;
+    secondElement.val = temp;
+  }
+
+  private static void inorder(TreeNode root) {
+    if (root == null) {
+      return;
+    }
+    inorder(root.left);
+    if (firstElement == null && (prev == null || prev.val >= root.val)) {
+      firstElement = prev;
+    }
+    if (firstElement != null && prev.val >= root.val) {
+      secondElement = root;
+    }
+    prev = root;
+    inorder(root.right);
+  }
+}
