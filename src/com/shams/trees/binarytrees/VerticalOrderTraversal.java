e683507aa6d2f5b596bb812bfad217a90a908dbc
i334072 <shams.nalband.thabrez@sap.com>
2021-05-21 19:15:37 +0530

    Adding vertical ordering

---
+++ src/com/shams/trees/binarytrees/VerticalOrderTraversal.java
@@ -0,0 +1,54 @@
+/**
+ * https://leetcode.com/problems/binary-tree-vertical-order-traversal/
+ * */
+package com.shams.trees.binarytrees;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class VerticalOrderTraversal {
+
+  public static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(3);
+    root.left = new TreeNode(9);
+    root.right = new TreeNode(20);
+    root.right.left = new TreeNode(15);
+    root.right.right = new TreeNode(7);
+
+    System.out.println(verticalOrder(root));
+  }
+
+  static List<List<Integer>> verticalOrder(TreeNode root) {
+    List<List<Integer>> result = new ArrayList<>();
+    verticalOrderUtil(root, 0);
+    columnToNodeMap.forEach((k, v) -> result.add(v));
+    return result;
+  }
+
+  static Map<Integer, List<Integer>> columnToNodeMap = new HashMap<>();
+  private static void verticalOrderUtil(TreeNode root, int col) {
+    if (root == null) {
+      return;
+    }
+    if (!columnToNodeMap.containsKey(col)) {
+      columnToNodeMap.put(col, new ArrayList<>());
+      columnToNodeMap.get(col).add(root.val);
+    } else {
+      columnToNodeMap.get(col).add(root.val);
+    }
+    verticalOrderUtil(root.left, col - 1);
+    verticalOrderUtil(root.right, col + 1);
+  }
+}
