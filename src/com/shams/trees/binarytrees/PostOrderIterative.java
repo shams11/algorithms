cc0a2e2fc5ef1b822d217ac764caf49666165359
i334072 <shams.nalband.thabrez@sap.com>
2021-05-21 11:00:55 +0530

    Adding binary tree pre post in order iterative traversals

--- src/com/shams/trees/binarytrees/PostOrderIterative.java
+++ src/com/shams/trees/binarytrees/PostOrderIterative.java
@@ -1,18 +1,17 @@
 /**
- * https://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion/
- * https://leetcode.com/problems/binary-tree-postorder-traversal/submissions/
+ * https://www.youtube.com/watch?v=sMI4RBEZyZ4&ab_channel=KevinNaughtonJr.KevinNaughtonJr.
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/
  * https://leetcode.com/problems/binary-tree-postorder-traversal/discuss/45551/Preorder-Inorder-and-Postorder-Iteratively-Summarization
  */
 
 package com.shams.trees.binarytrees;
 
 import com.shams.commons.UtilityHelper;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
-public class InorderIterative {
-
+public class PostOrderIterative {
   public static void main(String[] args) {
     UtilityHelper.Node root = new UtilityHelper.Node(1);
     root.left = new UtilityHelper.Node(2);
@@ -21,24 +20,24 @@ public class InorderIterative {
     root.left.right = new UtilityHelper.Node(5);
     root.right.left = new UtilityHelper.Node(6);
     root.right.right = new UtilityHelper.Node(7);
-    System.out.println(inorderIterative(root));
+    System.out.println(postOrderIterative(root));
   }
 
-  private static List<Integer> inorderIterative(UtilityHelper.Node root) {
-
+  private static List<Integer> postOrderIterative(UtilityHelper.Node root) {
     if (root == null) {
       return List.of();
     }
+    LinkedList<Integer> result = new LinkedList<>();
     Stack<UtilityHelper.Node> s = new Stack<>();
-    List<Integer> result = new ArrayList<>();
-    while (root != null || !s.isEmpty()) {
-      if (root != null) {
-        s.push(root);
-        root = root.left;
-      } else {
-        root = s.pop();
-        result.add(root.val);
-        root = root.right;
+    s.push(root);
+    while (!s.isEmpty()) {
+      root = s.pop();
+      result.addFirst(root.val);
+      if (root.left != null) {
+        s.push(root.left);
+      }
+      if (root.right != null) {
+        s.push(root.right);
       }
     }
     return result;
--- src/com/shams/trees/binarytrees/PreOrderIterative.java
+++ src/com/shams/trees/binarytrees/PreOrderIterative.java
@@ -1,9 +1,7 @@
 /**
- * https://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion/
- * https://leetcode.com/problems/binary-tree-postorder-traversal/submissions/
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/
  * https://leetcode.com/problems/binary-tree-postorder-traversal/discuss/45551/Preorder-Inorder-and-Postorder-Iteratively-Summarization
- */
-
+ * */
 package com.shams.trees.binarytrees;
 
 import com.shams.commons.UtilityHelper;
@@ -11,8 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class InorderIterative {
-
+public class PreOrderIterative {
   public static void main(String[] args) {
     UtilityHelper.Node root = new UtilityHelper.Node(1);
     root.left = new UtilityHelper.Node(2);
@@ -21,23 +18,22 @@ public class InorderIterative {
     root.left.right = new UtilityHelper.Node(5);
     root.right.left = new UtilityHelper.Node(6);
     root.right.right = new UtilityHelper.Node(7);
-    System.out.println(inorderIterative(root));
+    System.out.println(preIterative(root));
   }
 
-  private static List<Integer> inorderIterative(UtilityHelper.Node root) {
-
+  private static List<Integer> preIterative(UtilityHelper.Node root) {
     if (root == null) {
       return List.of();
     }
-    Stack<UtilityHelper.Node> s = new Stack<>();
     List<Integer> result = new ArrayList<>();
-    while (root != null || !s.isEmpty()) {
+    Stack<UtilityHelper.Node> s = new Stack<>();
+    while (!s.isEmpty() || root != null) {
       if (root != null) {
         s.push(root);
+        result.add(root.val);
         root = root.left;
       } else {
         root = s.pop();
-        result.add(root.val);
         root = root.right;
       }
     }
