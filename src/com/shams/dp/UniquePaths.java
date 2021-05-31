b6a0b916913e7c2f120b15cefe9cce1dae955c84
i334072 <shams.nalband.thabrez@sap.com>
2021-05-31 11:42:22 +0530

    Adding uniqe paths

---
+++ src/com/shams/dp/UniquePaths.java
@@ -0,0 +1,29 @@
+/**
+ * https://leetcode.com/problems/unique-paths/
+ */
+
+package com.shams.dp;
+
+public class UniquePaths {
+  public static void main(String[] args) {
+    int m = 3, n = 7;
+    System.out.println(uniquePaths(m, n));
+  }
+
+  public static int uniquePaths(int m, int n) {
+    int[][] t = new int[m][n];
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i == 0 || j == 0) {
+          t[i][j] = 1;
+        }
+      }
+    }
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        t[i][j] = t[i - 1][j] + t[i][j - 1];
+      }
+    }
+    return t[m - 1][n - 1];
+  }
+}
