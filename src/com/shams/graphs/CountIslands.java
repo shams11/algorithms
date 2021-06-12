c4072f6e7f7578186742eb3e5b8d981187a6ec58
i334072 <shams.nalband.thabrez@sap.com>
2021-06-12 16:02:27 +0530

    Refactored count islands

--- src/com/shams/graphs/CountIslands.java
+++ src/com/shams/graphs/CountIslands.java
@@ -43,12 +43,25 @@ public class CountIslands {
     for (int i = 0; i < m; i++) {
       for (int j = 0; j < n; j++) {
         if (grid[i][j] == '1') {
+          // count++;
+          // countIslands(grid, m, n, i, j);
           count++;
-          countIslands(grid, m, n, i, j);
+          dfs(grid, i, j);
+          }
         }
       }
-    }
     return count;
+  }
+
+  static void dfs(char[][] grid, int i, int j) {
+
+    if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') return;
+    grid[i][j] = '0';
+
+    dfs(grid, i - 1, j);
+    dfs(grid, i + 1, j);
+    dfs(grid, i, j + 1);
+    dfs(grid, i, j - 1);
   }
 
   static void countIslands(char[][] grid, int m, int n, int i, int j) {
