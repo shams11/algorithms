5f002904f9273d7202ddf5825990cf588da7e649
i334072 <shams.nalband.thabrez@sap.com>
2021-06-18 10:18:04 +0530

    Refactored jump game 2

--- src/com/shams/arrays/JumpGameII.java
+++ src/com/shams/arrays/JumpGameII.java
@@ -1,23 +1,26 @@
 /**
  * https://www.youtube.com/watch?v=BRnRPLNGWIo&ab_channel=AlgorithmsMadeEasy
  * https://leetcode.com/problems/jump-game-ii/
- * */
+ */
+
 package com.shams.arrays;
 
 public class JumpGameII {
   public static void main(String[] args) {
-    int[] a  = {2, 3, 1, 1, 4};
+    int[] a = {2, 3, 1, 1, 4};
     System.out.println(minJumps(a));
   }
 
-  private static int minJumps(int[] a) {
+  private static int minJumps(int[] nums) {
     int farthest = 0, end = 0, jumps = 0;
-    for(int i = 0; i < a.length - 1; i++) {
-      farthest = Math.max(farthest, i + a[i]);
-      if(i == end ) {
+    for (int i = 0; i < nums.length - 1; i++) {
+      farthest = Math.max(farthest, i + nums[i]);
+      if (farthest >= nums.length - 1) return jumps + 1;
+      if (i == end) {
         end = farthest;
         jumps++;
       }
+
     }
     return jumps;
   }
