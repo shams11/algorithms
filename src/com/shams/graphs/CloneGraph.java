e8cb59d9b6be2936a45e83c6f21b87b84fa7283b
i334072 <shams.nalband.thabrez@sap.com>
2021-06-27 19:55:31 +0530

    Refactored clone graph

--- src/com/shams/graphs/CloneGraph.java
+++ src/com/shams/graphs/CloneGraph.java
@@ -4,8 +4,47 @@
  * */
 package com.shams.graphs;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CloneGraph {
   public static void main(String[] args) {
 
+  }
+
+  public Node cloneGraph(Node node) {
+    if(node == null) return null;
+    Map<Integer, Node> map = new HashMap<>();
+    return cloneGraphUtil(node, map);
+  }
+
+  private Node cloneGraphUtil(Node node, Map<Integer, Node> map) {
+    if(map.containsKey(node.val)) return map.get(node.val);
+
+    Node newNode = new Node(node.val);
+    map.put(newNode.val, newNode);
+    for(Node n : node.neighbors) {
+      newNode.neighbors.add(cloneGraphUtil(n, map));
+    }
+    return newNode;
+  }
+
+  class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+      val = 0;
+      neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+      val = _val;
+      neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+      val = _val;
+      neighbors = _neighbors;
+    }
   }
 }
