0a477f0b9b1abb9b7610145ce711c51292c87b63
i334072 <shams.nalband.thabrez@sap.com>
2021-07-22 19:36:48 +0530

    Refactored largest number

--- src/com/shams/arrays/LargestNumber.java
+++ src/com/shams/arrays/LargestNumber.java
@@ -1,6 +1,7 @@
 /**
  * https://leetcode.com/problems/largest-number/
- * */
+ */
+
 package com.shams.arrays;
 
 import java.util.Arrays;
@@ -13,35 +14,25 @@ public class LargestNumber {
     System.out.println(largestNumber(nums));
   }
 
-  public static String largestNumber(int[] num) {
-    if (num == null || num.length == 0)
-      return "";
+  public static String largestNumber(int[] nums) {
+    String[] strArray = new String[nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      strArray[i] = String.valueOf(nums[i]);
+    }
 
-    // Convert int array to String array, so we can sort later on
-    String[] s_num = new String[num.length];
-    for (int i = 0; i < num.length; i++)
-      s_num[i] = String.valueOf(num[i]);
-
-    // Comparator to decide which string should come first in concatenation
-    Comparator<String> comp = (str1, str2) -> {
-      String s1 = str1 + str2;
-      String s2 = str2 + str1;
-      return s1.compareTo(s2); // reverse order here, so we can do append() later
+    Comparator<String> comp = (s1, s2) -> {
+      String str1 = s1 + s2;
+      String str2 = s2 + s1;
+      return str2.compareTo(str1);
     };
 
-    Arrays.sort(s_num, comp);
-    for(String j : s_num)
-    System.out.print(j + " ");
-    System.out.println();
+    Arrays.sort(strArray, comp);
     // An extreme edge case by lc, say you have only a bunch of 0 in your int array
-    if (s_num[0].charAt(0) == '0')
-      return "0";
-
+    if (strArray[0].equals("0")) return "0";
     StringBuilder sb = new StringBuilder();
-    for (String s : s_num)
+    for (String s : strArray) {
       sb.append(s);
-
+    }
     return sb.toString();
-
   }
 }
