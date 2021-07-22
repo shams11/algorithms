89a2ee8a4ce944e2760d6a67dbe5bae2738181e6
i334072 <shams.nalband.thabrez@sap.com>
2021-07-22 13:12:36 +0530

    Refactored container with most water

--- src/com/shams/algos/ContainerWithMostWater.java
+++ src/com/shams/algos/ContainerWithMostWater.java
@@ -6,6 +6,16 @@
 package com.shams.algos;
 
 public class ContainerWithMostWater {
+  /**Input: height = [1,8,6,2,5,4,8,3,7]
+   Output: 49
+   Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7].
+   In this case, the max area of water (blue section) the container can contain is 49.
+   *
+   *  i = 0 width = 8-0 = 8, len = [1], area = 7 max = 7
+   *     i = 1 width = 8-1 = 7, len = [7], area = 49 max = 49
+   *     j = 7 width = 7-1 = 6, len = [3], area = 18 max = 49
+   *     j = 6 width = 6-1 = 5, len = [8], area = 40 max = 40
+   * */
   public static void main(String[] args) {
     int[] a = {2, 3, 4, 5, 18, 17, 6};
     System.out.println(maxArea(a));
