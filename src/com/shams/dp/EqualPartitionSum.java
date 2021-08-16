fb33521028d47831a399523e94dc672ef5e39ed0
shams11 <shams.tabrez11@gmail.com>
2021-08-16 19:11:48 +0530

    Refactored equal sum partition

--- src/com/shams/dp/EqualPartitionSum.java
+++ src/com/shams/dp/EqualPartitionSum.java
@@ -20,7 +20,8 @@ public class EqualPartitionSum {
     if (sum % 2 != 0) {
       return false;
     }
-    return isSubSetSum(arr, sum / 2, n);
+    //return isSubSetSum(arr, sum / 2, n);
+    return canPartition(arr, sum/2, n);
   }
 
   // Recursive
@@ -35,5 +36,25 @@ public class EqualPartitionSum {
       return isSubSetSum(arr, sum, n - 1);
     }
     return isSubSetSum(arr, sum, n - 1) || isSubSetSum(arr, sum - arr[n - 1], n - 1);
+  }
+  private static boolean canPartition(int[] a, int sum, int n) {
+    boolean[][] t = new boolean[n+1][sum+1];
+    for(int i = 0; i < n + 1; i++) {
+      for (int j = 0; j < sum + 1; j++) {
+        if (i == 0) t[i][j] = false;
+        if (j == 0) t[i][j] = true;
+      }
+    }
+
+      for(int i = 1; i < n + 1; i++) {
+        for(int j = 1; j < sum + 1; j++) {
+          if(a[i - 1] > j) {
+            t[i][j] = t[i-1][j];
+          } else {
+            t[i][j] = t[i - 1][j - a[i-1]] || t[i - 1][j];
+          }
+        }
+      }
+   return t[n][sum];
   }
 }
