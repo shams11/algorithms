edaefa311f0f45cf4756f786a600f3073e9eda6f
shams11 <shams.tabrez11@gmail.com>
2021-08-17 10:54:09 +0530

    Number of good ways to split a string

--- src/com/shams/strings/NumOfGoodWaysToSplitString.java
+++ src/com/shams/strings/NumOfGoodWaysToSplitString.java
@@ -9,6 +9,14 @@ import java.util.Map;
 
 public class NumOfGoodWaysToSplitString {
   public static void main(String[] args) {
+    // Input: s = "aacaba"
+    //Output: 2
+    //Explanation: There are 5 ways to split "aacaba" and 2 of them are good.
+    //("a", "acaba") Left string and right string contains 1 and 3 different letters respectively.
+    //("aa", "caba") Left string and right string contains 1 and 3 different letters respectively.
+    //("aac", "aba") Left string and right string contains 2 and 2 different letters respectively (good split).
+    //("aaca", "ba") Left string and right string contains 2 and 2 different letters respectively (good split).
+    //("aacab", "a") Left string and right string contains 3 and 1 different letters respectively.
     String s = "aacaba";
     System.out.println(numSplits(s));
   }
