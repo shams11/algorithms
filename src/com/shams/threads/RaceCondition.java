f6812050295677a15c0962412cc8538e38d98bb1
shams11 <shams.tabrez11@gmail.com>
2021-08-21 20:23:27 +0530

    Refactored race condition code

--- src/com/shams/threads/RaceCondition.java
+++ src/com/shams/threads/RaceCondition.java
@@ -1,5 +1,7 @@
 package com.shams.threads;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class RaceCondition {
   public static void main(String[] args) {
     Runnable runnable = new MyRunnable();
@@ -11,18 +13,28 @@ public class RaceCondition {
 
   public static class MyRunnable implements Runnable {
     // shared variable by 2 threads
-    int count = 0;
+     int count = 0;
+    AtomicInteger value = new AtomicInteger(0);
     @Override
     public void run() {
       for(int i = 0; i < 1000000; ++i) {
         // without synchronized, no thread will reach to 2 million
         // because of write visibility problem
 
+        // case 1 : without synchronized block
+        this.count++;
+
         // with synchronized, one of the threads will reach to 2 million
-        synchronized (this) {
-          this.count++;
-        }
+        // case 2 : without synchronized block
+//        synchronized (this) {
+//          this.count++;
+//        }
+
+        // case 1 : With Atomic Integer
+        // value.incrementAndGet();
       }
+      // System.out.println(Thread.currentThread().getName() + " : " + count);
+      // System.out.println(Thread.currentThread().getName() + " : " + value);
       System.out.println(Thread.currentThread().getName() + " : " + count);
     }
   }
