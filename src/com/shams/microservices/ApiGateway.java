2834669c9afc1751962ac08754b97636ba1bbe3e
shams11 <shams.tabrez11@gmail.com>
2021-08-22 20:00:08 +0530

    Adding microservices api gateway

---
+++ src/com/shams/microservices/ApiGateway.java
@@ -0,0 +1,19 @@
+package com.shams.microservices;
+
+public class ApiGateway {
+  // API Gateway
+  //---------
+  //- Security
+  //- Authenticatiom
+  //- Authorization
+  //- Cache (Trending service)
+  //- LB
+  //- static files
+  //- canary deployemnt (route 5 % of traffic)
+  //- Rate limiting
+  //- Routing to specific service based on url (Mobile or desktop app)
+  //- Single netwoek call instead of multiple calls to mutiple serviecs
+  //- Billing for api calls from clients
+  //
+  //ex : api gee, nginx, aws api gateway, azure, google endpoints
+}
