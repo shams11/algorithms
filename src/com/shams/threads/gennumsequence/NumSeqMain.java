992d5f2f75d6ed0bf298081207f369e1646844f1
shams11 <shams.tabrez11@gmail.com>
2021-08-23 08:16:30 +0530

    Refactored generate numbers in sequence using multiple threads

--- src/com/shams/threads/gennumsequence/NumSeqMain.java
+++ src/com/shams/threads/gennumsequence/NumSeqMain.java
@@ -15,4 +15,25 @@ public class NumSeqMain {
     t2.start();
     t3.start();
   }
+
+  // Output : Thread 1 1
+  //Thread 2 2
+  //Thread 3 3
+  //Thread 1 4
+  //Thread 2 5
+  //Thread 3 6
+  //Thread 1 7
+  //Thread 2 8
+  //Thread 3 9
+  //Thread 1 10
+  //Thread 2 11
+  //Thread 3 12
+  //Thread 1 13
+  //Thread 2 14
+  //Thread 3 15
+  //Thread 1 16
+  //Thread 2 17
+  //Thread 3 18
+  //Thread 1 19
+  //Thread 2 20
 }
