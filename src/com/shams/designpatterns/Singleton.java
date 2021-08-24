94b06944a70394e4d71e6aa4b36802b66489e06d
shams11 <shams.tabrez11@gmail.com>
2021-08-24 13:22:15 +0530

    Adding singleton pattern

---
+++ src/com/shams/designpatterns/Singleton.java
@@ -0,0 +1,22 @@
+package com.shams.designpatterns;
+
+public class Singleton {
+
+  private static volatile Singleton obj = null;
+
+  private Singleton() {
+  }
+
+  public static Singleton getInstance() {
+    if (obj == null) {
+      // To make thread safe
+      synchronized (Singleton.class) {
+        // check again as multiple threads
+        // can reach above step
+        if (obj == null)
+          obj = new Singleton();
+      }
+    }
+    return obj;
+  }
+}
