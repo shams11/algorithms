c79008b0e4fc4911681481759903ac9169bad34b
shams11 <shams.tabrez11@gmail.com>
2021-09-25 09:24:04 +0530

    Refactored MinDaysToFlowerBloom

--- src/com/shams/binarysearch/MinDaysToFlowerBloom.java
+++ src/com/shams/binarysearch/MinDaysToFlowerBloom.java
@@ -5,6 +5,8 @@
 
 package com.shams.binarysearch;
 
+import java.util.Arrays;
+
 public class MinDaysToFlowerBloom {
   public static void main(String[] args) {
     // [1,10,3,10,2]
@@ -25,20 +27,16 @@ public class MinDaysToFlowerBloom {
 
   }
 
-  public static int minDays(int[] bloomDay, int m, int k) {
-    int n = bloomDay.length;
+  public static int minDays(int[] bloomDay, int totalBouquets, int k) {
+    int totalFlowers = bloomDay.length;
     int res = -1;
-    // if((m * k)  > n) {
-    //     return -1;
-    // }
-    int start = 1, end = Integer.MIN_VALUE, mid;
-    for (int i = 0; i < n; i++) {
-      end = Math.max(end, bloomDay[i]);
-    }
-
+    int start = 1, end, mid;
+    end = Arrays.stream(bloomDay).max().getAsInt();
     while (start <= end) {
       mid = start + (end - start) / 2;
-      if (isValidScheme(bloomDay, n, k, m, mid)) {
+      if (isValidScheme(bloomDay, totalFlowers, k, totalBouquets, mid)) {
+        // mid is one of the possible answers. there can be other possible answers which needs
+        // to be evaluated further
         res = mid;
         end = mid - 1;
       } else {
@@ -54,13 +52,14 @@ public class MinDaysToFlowerBloom {
   // mid = (7 + 12) / 2 = 9, then start = 10
   // mid = (10 + 12) / 2 = 11, then start = 12
   // mid = (12 + 12) / 2 = 12, then isValidScheme returns true, 12 is the answer
-  static boolean isValidScheme(int[] bloomDay, int n, int k, int totalBouquets, int maxDay) {
+  static boolean isValidScheme(int[] bloomDay, int totalFlowers, int k, int totalBouquets, int maxDay) {
     int curBloomFlowerCount = 0;
     int curBouquetCount = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < totalFlowers; i++) {
       if (bloomDay[i] <= maxDay) {
         curBloomFlowerCount++;
       } else {
+        // since it has to be adjacent flowers
         curBloomFlowerCount = 0;
       }
       if (curBloomFlowerCount == k) {
