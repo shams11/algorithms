047019239facbf79e6c90ba1ce92180e25da75d1
shamsthabrez <shamsthabrez@microsoft.com>
2023-01-29 17:43:01 +0530

    Added comments

--- src/com/shams/arrays/InsertIntervals.java
+++ src/com/shams/arrays/InsertIntervals.java
@@ -13,19 +13,20 @@ public class InsertIntervals {
     // Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
     // Output: [[1,2],[3,10],[12,16]]
     // Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
-    int[][] intervals = {{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}};
-    int[] newInterval = {4, 8};
+    int[][] intervals = { { 1, 2 }, { 3, 5 }, { 6, 7 }, { 8, 10 }, { 12, 16 } };
+    int[] newInterval = { 4, 8 };
     System.out.println(Arrays.deepToString(insert(intervals, newInterval)));
   }
 
   public static int[][] insert(int[][] intervals, int[] newInterval) {
 
-    //if(intervals.length == 0) return new int[]{newInterval};
+    // if(intervals.length == 0) return new int[]{newInterval};
 
     List<int[]> result = new ArrayList<>();
     for (int[] interval : intervals) {
       if (interval[0] > newInterval[1]) {
         // Non-overlapping
+        // ex : in {12, 16}, 12 is greater than 8 in {4,8}
         result.add(newInterval);
         newInterval = interval; // start over again from this interval
       } else if (interval[1] < newInterval[0]) {
